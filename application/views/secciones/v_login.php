202a338b47d3b39220652abc154a44bce22df840
astroSolution <j-e_1997@hotmail.es>
2016-11-12 08:50:50 -0400

    inicio de proyeccto

---
+++ application/views/secciones/v_login.php
@@ -0,0 +1,17 @@
+<section id="login">
+  <div class="container">
+    <form class="form-horizontal" action="<?php echo base_url('Usureg/login');?>" method="post">
+      <div class="form-group input-group">
+        <span class="input-group input-group-addon"></span>
+        <input type="text" name="usuario" class="form-control"/>
+      </div>
+      <div class="form-group input-group">
+        <span class="input-group input-group-addon"></span>
+        <input type="text" name="usuario" class="form-control"/>
+      </div>
+      <div class="form-group input-group">
+        <button type="submit" name="loguear">Loguear</button>
+      </div>
+    </form>
+  </div>
+</section>
